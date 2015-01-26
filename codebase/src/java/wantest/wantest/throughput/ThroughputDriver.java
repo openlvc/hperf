ffec14f7f5ae77d2888cb9389d45eecd15440ac4
Tim Pokorny <tim.pokorny@calytrix.com>
2015-01-26 20:48:30 +0800

    Fix #7 - I thought I had fixed this before, but from looking at some of the results I think there was a loss of precision due to a conversion not happening properly. Should be fixed now.

--- codebase/src/java/wantest/wantest/throughput/ThroughputDriver.java
+++ codebase/src/java/wantest/wantest/throughput/ThroughputDriver.java
@@ -22,9 +22,7 @@ package wantest.throughput;
 
 import static wantest.Handles.*;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
