8187272ebd95d8295d608fa9d448c35073c3a32b
Tim Pokorny <tim.pokorny@calytrix.com>
2015-02-24 21:31:05 +0800

    Updated the command line help for the latency federate to provide some context as well as an example invocation command.

--- codebase/resources/dist/common/latency.sh
+++ codebase/resources/dist/common/latency.sh
@@ -2,6 +2,15 @@
 
 function printUsage()
 {
+	echo ""
+	echo "The latency test is run with any number of federates. Only one federate is"
+	echo "designated as the sender. The others are all receivers/responders. The    "
+	echo "sender instigates each loops by sending a Ping interaction, to which all  "
+	echo "responders reply with a PingAck"
+	echo ""
+	echo " NOTE: Bundling can have an adverse effect on latency performance. Turn it"
+	echo "       off in the RTI.rid file before continuing."
+	echo ""
 	echo "usage: latency.sh [--arg <value>]"
 	echo ""
 	echo "    --federate-name      [stirng]    (REQUIRED) Name for this federate"
@@ -9,10 +18,13 @@ function printUsage()
 	echo "    --loops              [number]    (optional) Number of loops we should iterate for, default 20"
 	echo "    --peers              [list]      (REQUIRED) Comma-separated list of other federate names"
 	echo "    --packet-size        [number]    (optional) Min size of messages. e.g. 1B, 1K, 1M, default 1K"
+	echo "    --sender                         (optional) Is this federate the one event sender, default false"
 	echo "    --validate-data                  (optional) Validate received contents and log any errors, default false"
 	echo "    --log-level          [string]    (optional) TRACE | DEBUG | INFO | WARN | FATAL | OFF, default INFO"
 	echo "    --print-event-log                (optional) If specified, prints a list of vital stats on all events"
 	echo "    --export-cvs         [string]    (optional) File to dump event list to. No export if not provided"
+	echo ""
+	echo "example: ./latency-sh --federate-name one --peers two,three --loops 10000 --sender"
 	echo ""
 	exit;
 }
